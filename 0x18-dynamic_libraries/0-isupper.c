3da8266e6323df4f6222f8452f7ffbad5b971905
Harrison <ugochukwujohn2014@gmail.com>
2022-07-12 14:21:14 -0700

    ALX Task 0

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _isupper - This is a function that checks for uppercase character
+ * @c: is the character to print
+ * Author: Harrison U. John
+ * Return: 1 if c is uppercase
+ * Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
